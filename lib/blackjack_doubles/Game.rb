76b75a9bfd8221c52a39ad8364c60af69a77fece
@restc <restc@icloud.com>
2014-04-06 12:36:43 -0700

    Changed some gameplay text

--- lib/blackjack_doubles/Game.rb
+++ lib/blackjack_doubles/Game.rb
@@ -11,6 +11,7 @@ module BlackjackDoubles
     def initialize(names=nil, first_run=true)
       case first_run
       when true
+        system 'clear'
         unless self.read_instructions? == false
           self.read_instructions
         end
@@ -172,21 +173,25 @@ module BlackjackDoubles
       system 'clear'
       puts <<-PARAGRAPH
 
-      BlackJackDoubles is a spinoff of the popular card game BlBlackJackDoubles.
+      BlackJackDoubles is a spinoff of the popular card game Blackjack.
       It requires two players to play, and a neutral dealer.
 
       When starting the game, both players set an initial bet.
       They are then dealt two cards each, face up, so that both
       may see the other player's hand. A hand is any collection of cards.
-      Like BlBlackJackDoubles, you win if you score 21 points, or if you have a
+      Like Blackjack, you win if you score 21 points, or if you have a
       higher score than the other player's corresponding hand while staying
-      under 21. You may have as many hands as you'd like.
+      under 21.
 
-      However, when you split a hand, your bet automatically doubles.
+      If you are dealt a duplicate card to your hand, your hand will
+      automatically split. When you split a hand, your bet also doubles.
       At the end of the game, whether you stop betting or you run out of cards,
       your hands will be compared to your opponent's hands, the best against the
-      best, and the winner will receive the prize money.
-      You'll both start out with 1000g, and the minimum bet is 20g. Good luck!
+      best, and the winner will receive the prize money, with a bonus if you've
+      played more than one hand that didn't bust.
+
+      You'll both start out with 1000g, and the minimum bet to play is 20g.
+      Good luck!
 
 
       * Press [Enter] to continue *
@@ -442,6 +447,7 @@ module BlackjackDoubles
       puts "\n\nPlay again?"
       continue = gets.chomp
       if ['n', 'no'].include? continue
+        self.winning_score
         exit
       elsif ['y', 'yes'].include? continue
         self.reset_game
